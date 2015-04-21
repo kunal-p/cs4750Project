ba217e3bfdf0971f7bdb173f72a483e5cf731e72
kp2ef <kp2ef@labunix01.cs.virginia.edu>
2015-04-21 14:30:31 -0400

    update landing page

--- patient.php
+++ patient.php
@@ -1,3 +1,18 @@
+<?php
+session_start();//starting session
+$id;
+if(isset($_SESSION['id'])){
+        if($_SESSION['type'] == 0){
+                $id = $_SESSION['id'];
+        }else{
+                header('Location: index.html');
+        }
+} else{
+        header('Location: index.html');
+}
+?>
+
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -22,7 +37,7 @@
 
         <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
         <script src="js/javascript.js"></script>
-    </head>
+   </head>
     <body>
 
 
@@ -42,7 +57,7 @@
         <div class="panel-body">
           <div class="row">
             <div class="col-xs-12 col-sm-4 text-center">
-              <img src="http://api.randomuser.me/portraits/women/21.jpg" alt="" class="center-block img-circle img-thumbnail img-responsive">
+              <img src="http://d13beo3f7vpmvd.cloudfront.net/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" alt="" class="center-block img-circle img-thumbnail img-responsive">
               <ul class="list-inline ratings text-center" title="Ratings">
                 <li><a href="#"><span class="fa fa-star fa-lg"></span></a></li>
                 <li><a href="#"><span class="fa fa-star fa-lg"></span></a></li>
@@ -53,15 +68,69 @@
             </div>
             <!--/col--> 
             <div class="col-xs-12 col-sm-8">
-              <h2>Jane Doe</h2>
-              <p><strong>About: </strong> Web Designer / UI Expert. </p>
-              <p><strong>Hobbies: </strong> Read, out with friends, listen to music, draw and learn new things. </p>
-              <p><strong>Skills: </strong>
-                <span class="label label-info tags">html5</span> 
-                <span class="label label-info tags">css3</span>
-                <span class="label label-info tags">jquery</span>
-                <span class="label label-info tags">bootstrap3</span>
-              </p>
+              
+		<?php
+		include_once("./users.php");
+		$db_connection = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+		if (mysqli_connect_errno()) {
+			echo("Can't connect to MySQL Server. Error code: " .  mysqli_connect_error());
+			return null;
+		}
+		$stmt = $db_connection->stmt_init();
+ 		if($stmt->prepare("SELECT * FROM patient WHERE patient_id='$id'") or die("<br/>Error Building Query!<br/>" . mysqli_error($db_connection))) {
+    			$stmt->execute();
+    			$stmt->bind_result($blood_type, $weight, $height, $patient_id, $first_name, $last_name, $middle_name, $email, $dob, $address);
+			while($stmt->fetch()){
+				echo "<table class=\"table\">";
+		        	echo "<tr><td><b>Patient ID:</b></td> <td>$patient_id</td> </tr>";
+		       		echo " <tr><td><b>First Name:</b></td> <td>$first_name</td> </tr>";
+		     	   	echo " <tr><td><b>Middle Name:</b></td><td>$middle_name</td> </tr>";
+		    	    	echo " <tr><td><b>Last Name:</b></td> <td>$last_name</td> </tr>";
+		        	echo " <tr><td><b>Blood Type:</b></td> <td>$blood_type</td> </tr>";
+		        	echo " <tr><td><b>Weight:</b></td> <td>$weight</td> </tr>";
+		        	echo " <tr><td><b>Height:</b></td> <td>$height</td> </tr>";
+       		 		echo " <tr><td><b>DOB:</b></td> <td>$dob</td> </tr>";
+				echo " <tr><td><b>Email:</b></td> <td>$email</td> </tr>";
+			        echo " <tr><td><b>Address:</b></td> <td>$address</td> </tr>";
+				
+				if($stmt->prepare("SELECT medication FROM medication WHERE patient_id='$id'") or die("<br/>Error Building Query!<br/>" . mysqli_error($db_connection))) {
+		         		$stmt->execute();
+          				$stmt->bind_result($medication);
+          				echo " <tr><td><b>Medication:</b></td></tr>";
+          				while ($stmt->fetch()) {
+            					echo "<tr><td></td><td>$medication</td></tr>";
+          				}
+        			}		
+        			if($stmt->prepare("SELECT allergy FROM allergy WHERE patient_id='$id'") or die("<br/>Error Building Query!<br/>" . mysqli_error($db_connection))) {
+          				$stmt->execute();
+          				$stmt->bind_result($allergy);
+          				echo " <tr><td><b>Allergies:</b></td></tr>";
+          				while ($stmt->fetch()) {
+            					echo "<tr><td></td><td>$allergy</td></tr>";
+          				}
+				}
+        			if($stmt->prepare("SELECT phone_number FROM patient_phone WHERE patient_id='$id'") or die("<br/>Error Building Query!<br/>" . mysqli_error($db_connection))) {
+          				$stmt->execute();
+          				$stmt->bind_result($phone);
+          				echo " <tr><td><b>Phone:</b></td></tr>";
+          				while ($stmt->fetch()) {
+            					echo "<tr><td></td><td>$phone</td></tr>";
+          				}
+        			}
+
+
+
+
+
+				
+				echo "</table>";
+     			}
+    			$stmt->close();
+  		}
+  		$db_connection->close();
+		?>
+
+
             </div>
             <!--/col-->          
             <div class="clearfix"></div>
